5c536fb2a5034475ec31f1982cc47fe83769ee92
Devlon Duthie <duthied@gmail.com>
2013-07-14 10:44:16 -0700

    added Post model edit, some styling

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -7,12 +7,28 @@ class PostsController < ApplicationController
   def new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
   def create
     # render text: params[:post].inspect
     @post = Post.new(post_params)
-    @post.save
-
-    redirect_to @post
+    
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
