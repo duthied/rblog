a877f2d8641e109439d44c63ac72b1fda0820a13
Devlon Duthie <duthied@gmail.com>
2013-08-03 16:59:26 -0700

    added api error handling comments + rel link to comment json template

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,26 @@
 class CommentsController < ApplicationController
-  
+  skip_before_action :verify_authenticity_token
+
   #PUBLIC METHODS
   def create
     @post = Post.find(params[:post_id])
+    # TODO: throw not found if not found?
+
     @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
-    redirect_to post_path(@post)
+
+    # redirect_to post_path(@post)
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to post_path(@post) }
+        format.json { render 'index' }
+      end
+    else
+      respond_to do |format|
+        format.html { render 'posts/show' }
+        format.json { render 'create', :status => :bad_request }
+      end
+    end
+
   end
 
   def destroy
--- app/views/comments/index.json.rabl
+++ app/views/comments/index.json.rabl
@@ -1,3 +1,8 @@
 object @comment
 attributes :commenter, :body
 attribute :created_at => :create_date, :updated_at => :update_date
+
+node(:links) do |comment|
+  {:rel => "self",
+   :href => post_comment_path(comment.post, comment)}
+end
