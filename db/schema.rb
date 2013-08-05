0e1dafdfe88842cbbc245fc727c15ca7cb3856da
Devlon Duthie <duthied@gmail.com>
2013-08-05 10:06:03 -0700

    added partial for user form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130804230103) do
+ActiveRecord::Schema.define(version: 20130805161234) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20130804230103) do
 
   create_table "roles", force: true do |t|
     t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_roles", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
